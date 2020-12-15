dfa04f5a4cd0613859ff0a1dc477f4b63cefc8ac
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-16 00:54:29 +0800

    Wave background

---
+++ app/src/Components/General/Wave.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styled from "styled-components";
+import { ReactComponent as SVG } from "../../static/wave.svg";
+const StyledWave = styled.div`
+  z-index: -1;
+  position: fixed;
+  top: 0;
+  right: 0;
+  width: max(400px, 40vw);
+  .z {
+    transform: rotate(180deg);
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    width: min(350px, 40vw);
+  }
+`;
+
+export const Wave: React.FC = () => {
+  return (
+    <StyledWave>
+      <SVG />
+      <div className="z">
+        <SVG />
+      </div>
+    </StyledWave>
+  );
+};
