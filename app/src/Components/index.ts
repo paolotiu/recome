7dcc88d0c8d43275953805f20327dbaf49ef85c2
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-16 17:33:30 +0800

    Added home and header

--- app/src/Components/index.ts
+++ app/src/Components/index.ts
@@ -1,3 +1,5 @@
 export { Login } from "./Login/Login";
 export { Recommend } from "./Recommend/Recommend";
 export { Wave } from "./General";
+export { Home } from "./Home/Home";
+export { Header } from "./Header/Header";
