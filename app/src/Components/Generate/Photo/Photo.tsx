96555ee9bf05a22d431604c5d7fd784c9cda1304
paolotiu17 <61054234+paolotiu17@users.noreply.github.com>
2020-12-25 16:54:46 +0800

    removed warnings

--- app/src/Components/Generate/Photo/Photo.tsx
+++ app/src/Components/Generate/Photo/Photo.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import styled from "styled-components";
 import { ResultArtist, ResultTrack } from "../../../types";
 import { v4 as uuid } from "uuid";
